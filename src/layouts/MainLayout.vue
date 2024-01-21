5aa96ee575fb5c803d678804e9a30dab06463fef
Artmon29 <dresgachi49@gmail.com>
2024-01-21 11:10:28 -0300

    Plantilla base finalizada

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+    
+    <div class="wrapper">
+        <NavBar/>
+        <!-- <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        </div> -->
+        <SideBar/>
+
+        <div class="content-wrapper"><!-- conetnt-wrapper  por ver -->
+            <router-view/>
+        </div><!-- conetnt-wrapper  por ver -->
+
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.2.0
+        </div>
+    </footer>
+
+    <aside class="control-sidebar control-sidebar-dark">
+
+    </aside>
+
+    </div>
+</template>
+<script>
+import NavBar from './inc/NavBar'
+import SideBar from './inc/Sidebar'
+
+ export default{
+    components:{
+        NavBar,
+        SideBar
+    }
+ }
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <content-header title="About" icon="fas fa-info-circle"/>
+  <content>
+    <div class="row">
+      <div class="col-6">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">About</h3>
+          </div>
+          <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing
+          </div>
+          <div class="card-footer">
+            footer
+          </div>
+        </div>
+      </div>
+    </div>
+  </content>
 </template>
+
